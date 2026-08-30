f391e35f4cfec235b0a918cf1722ef1c81e7d019
Andrea Pantelić <153278943+andreapantelic@users.noreply.github.com>
2026-08-30 14:04:41 +0200

    Create OurBlockPlacementPass.h

---
+++ OurBlockPlacementPass/OurBlockPlacementPass.h
@@ -0,0 +1,10 @@
+#ifndef LLVM_OURBLOCKPLACEMENTPASS_H
+#define LLVM_OURBLOCKPLACEMENTPASS_H
+
+#include "llvm/IR/Function.h"
+
+using namespace llvm;
+
+bool runOurBlockPlacement(Function &F);
+
+#endif
