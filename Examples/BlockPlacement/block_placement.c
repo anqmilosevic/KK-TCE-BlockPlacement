22bd0f9f49d1782c295bae2f5e984181818bba06
Andrea Pantelić <153278943+andreapantelic@users.noreply.github.com>
2026-08-30 14:08:06 +0200

    Add files via upload

---
+++ Examples/BlockPlacement/block_placement.c
@@ -0,0 +1,58 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int check(int n) {
+  if (n < 0) {
+    printf("Greska: negativna vrednost!\n");
+    exit(1);
+  }
+
+  return n * 2;
+}
+
+int sum(int n) {
+  int s = 0;
+
+  for (int i = 0; i < n; i++) {
+    s = s + check(i);
+  }
+
+  return s;
+}
+
+int process(int *a, int n) {
+  int s = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (a[i] < 0) {
+      printf("Greska: neispravan element!\n");
+      abort();
+    }
+
+    s = s + a[i];
+  }
+
+  return s;
+}
+
+void ispisi(bool detaljno) {
+  if (detaljno) {
+    printf("detaljan ispis\n");
+  }
+  else {
+    printf("kratak ispis\n");
+  }
+}
+
+int main() {
+  int a[5] = {1, 2, 3, 4, 5};
+
+  printf("sum(10) = %d\n", sum(10));
+  printf("process(a, 5) = %d\n", process(a, 5));
+
+  ispisi(true);
+  ispisi(false);
+
+  return 0;
+}
